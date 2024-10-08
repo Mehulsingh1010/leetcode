b953420daeb6830c297d38c5e2013703d45cdb0d
Mehul Singh <mehulsingh076@gmail.com>
2024-10-09 00:07:12 +0530

    Time: 6 ms (16.58%), Space: 46.2 MB (98.96%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,11 +1,14 @@
+import java.util.HashMap;
+
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < numbers.length; i++) {
-            for (int j = i + 1; j < numbers.length; j++) {
-                if (numbers[i] + numbers[j] == target) {
-                    return new int[] { i+1, j+1 };
-                }
+            int complement = target - numbers[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement) + 1, i + 1 }; // Adding 1 to each index
             }
+            map.put(numbers[i], i);
         }
         throw new IllegalArgumentException("No two sum solution");
     }
