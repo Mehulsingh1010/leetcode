51f753d8947909f03341bc5d11071b1cb558c2f3
Mehul Singh <mehulsingh076@gmail.com>
2024-10-09 22:24:19 +0530

    Time: 0 ms (100%), Space: 42.2 MB (8.44%) - LeetHub

--- 0748-largest-number-at-least-twice-of-others/0748-largest-number-at-least-twice-of-others.java
+++ 0748-largest-number-at-least-twice-of-others/0748-largest-number-at-least-twice-of-others.java
@@ -1,21 +1,26 @@
 class Solution {
     public int dominantIndex(int[] nums) {
-        if (nums.length == 0) return -1;
-        
-        int maxIndex = 0;
-        
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] > nums[maxIndex]) {
-                maxIndex = i;
+        int n = nums.length;
+        if (n < 2) return -1; // Handle edge case
+
+        int maxIndex = 0; // Pointer for the index of the maximum value
+        int maxValue = nums[0]; // Initialize maxValue
+
+        // First, find the maximum value and its index
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > maxValue) {
+                maxValue = nums[i];
+                maxIndex = i; // Update maxIndex
             }
         }
-        
-        for (int i = 0; i < nums.length; i++) {
-            if (i != maxIndex && nums[maxIndex] < 2 * nums[i]) {
-                return -1;
+
+        // Now check if the maxValue is at least twice the value of all other numbers
+        for (int i = 0; i < n; i++) {
+            if (i != maxIndex && maxValue < 2 * nums[i]) {
+                return -1; // Not dominant
             }
         }
-        
-        return maxIndex;
+
+        return maxIndex; // Return the index of the dominant value
     }
 }
